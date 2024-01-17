d1da65cd3c29085dce2b359d068484f407f90e98
jhon <jhon.bocanegra26@gmail.com>
2024-01-17 09:29:04 -0500

    update: CSS and HTML code

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="es">
+      <body className={inter.className}>
+        <div className="absolute top-0 -z-10 h-full w-full bg-white">
+          <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(51,152,219,0.5)] opacity-50 blur-[80px]" />
+        </div>
+        {children}
+        </body>
     </html>
   )
 }
